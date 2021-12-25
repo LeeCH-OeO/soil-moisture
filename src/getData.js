b09ab44450b75700f4b1dea25d5db8bc1ecb20ec
LeeCH-OeO <fairprice81000@gmail.com>
2021-12-25 21:10:18 +0800

    update areaChart

--- src/getData.js
+++ src/getData.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getDatabase, ref, onValue} from "firebase/database";
 import myFirebase from "./firebase";
 import Typography from '@mui/material/Typography';
-import { typography } from "@mui/system";
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
 
 const database = getDatabase(myFirebase)
 
@@ -14,22 +14,28 @@ export default function GetData(){
         const getNumber = snapshot.val()
         const numberList = []
         for(let id in getNumber){
-            numberList.push(getNumber[id].moisture)
+            numberList.push(getNumber[id])
         }
         setSensorData(numberList)
-        console.log()
+        
         })
     }, [])
     return(
         
         <div>
             <div className="currentData">
-                <Typography variant="h3" gutterBottom>Current: {sensorData[sensorData.length-1]}</Typography>
+                <Typography variant="h3" gutterBottom>Current: { sensorData ? sensorData[sensorData.length-1].moisture  :""}</Typography>
             </div>
 
             <div className="allData">
-                <Typography variant="h3" gutterBottom>History </Typography>
-                {sensorData ? sensorData.map((i)=> <Typography variant="h6" >{i}</Typography>):""}
+                
+                    <AreaChart width={1800} height={800} data={sensorData}>
+                    <Area type="monotone" dataKey="moisture" stroke="#8884d8" activeDot={{ r: 3 }}/>
+                    <CartesianGrid stroke="#ccc" />
+                    <XAxis dataKey="time"/>
+                    <YAxis dataKey="moisture"/>
+                    <Tooltip />
+                    </AreaChart>             
             </div>
             
         </div>
