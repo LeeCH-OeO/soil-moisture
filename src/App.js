ed50b5ad8608bb8a0628da16861ab3312b40ff8c
LeeCH-OeO <fairprice81000@gmail.com>
2021-12-26 07:26:12 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import ErrorPage from './errorPage';
 import NewData from "./new";
 import {GetData} from "./getCurrentData";
-import { GetChart } from './getCahrt';
+import { GetChart } from './getChart';
 import GetAllData from './getAllData';
+
 import "./App.css"
+import { ButtonGroup, Button } from '@mui/material';
 function App() {
 
   return (
     <BrowserRouter>
-      <Typography className='header' variant="h1" gutterBottom style={{'fontFamily': 'Noto Serif TC', 'color':'red'}} >土壤濕度監測</Typography>
-      <NewData/>
-      <nav>
-        <Link to="/soil-moisture">Home Page</Link>
-        <Link to="/soil-moisture/Chart" > Chart</Link>
-        <Link to="/soil-moisture/History" > History</Link>
-      </nav>
+      <div className='header'>
+        <Typography  variant="h1" gutterBottom style={{'fontFamily': 'Noto Serif TC', 'color':'red'}} >土壤濕度監測</Typography>
+        <NewData/>
+        <ButtonGroup variant="outlined">
+          <Button><Link to="/soil-moisture">Home Page</Link></Button>
+          <Button><Link to="/soil-moisture/Chart" > Chart</Link></Button>
+          <Button><Link to="/soil-moisture/History" > History</Link></Button>
+        </ButtonGroup>
+      </div> 
       <Routes>
         <Route path="/soil-moisture" element={<GetData/>} />
         <Route path="/soil-moisture/Chart" element={<GetChart/>} />
         <Route path="/soil-moisture/History" element={<GetAllData/>} />
         <Route path="/soil-moisture/*" element={<ErrorPage/>} />
       </Routes>
+      <div className='footer'>
+        <a href="https://github.com/LeeCH-OeO/soil-moisture" target="_blank" rel="noreferrer">
+          <Typography variant='subtitle2'>©LeeCH-OeO</Typography> 
+        </a>
+      </div>
     </BrowserRouter>
     
   );
