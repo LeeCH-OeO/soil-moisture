40f28a8da99cbbe6ff662efd8703ec502cff6993
LeeCH-OeO <fairprice81000@gmail.com>
2021-12-25 21:09:25 +0800

    add push timeStamp

--- src/new.js
+++ src/new.js
@@ -5,19 +5,23 @@ import { getDatabase, ref, push, } from "firebase/database";
 const database = getDatabase(myFirebase)
 export default function NewData(){
     const [number, setNumber] = useState('')
-    const handleOnChange = (e)=>{
+    const handleDataOnChange = (e)=>{
         setNumber(e.target.value)
+       
     }
+    
     const createNumber = ()=>{
+        
         push(ref(database, 'data/'), {
             moisture: number,
-            
+            time: Date()            
         })
     }
 
     return(
         <div>
-            <input type="text" onChange={handleOnChange} value={number}></input>
+            <input type="text" onChange={handleDataOnChange} value={number}></input>
+            
             <button onClick={createNumber}>add number</button>
         </div>
     )
