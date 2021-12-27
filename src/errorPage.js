f3bc12bcf18b47a30a5dacc9b51ba575edd35774
LeeCH-OeO <fairprice81000@gmail.com>
2021-12-28 03:13:23 +0800

    update error page

--- src/errorPage.js
+++ src/errorPage.js
@@ -1,9 +1,9 @@
 import React from "react";
 export default  function ErrorPage(){
     return(
-        <div>
-            <h3>Error!</h3>
-            <img src="https://walfiegif.files.wordpress.com/2021/04/out-transparent-5.gif?w=1000" ></img>
+        <div className="error">
+            <h3 style={{'fontFamily': 'Noto Sans  TC', 'fontWeight': '900', 'color':'white'}}>Error!</h3>
+            <img src="https://walfiegif.files.wordpress.com/2021/04/out-transparent-5.gif?w=600" ></img>
         </div>
     )
 }
