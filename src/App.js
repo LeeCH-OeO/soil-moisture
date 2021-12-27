ee6720bd3707de17f4a35efd82ad744a222b553c
LeeCH-OeO <fairprice81000@gmail.com>
2021-12-28 03:15:01 +0800

    update firebase hosting and HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Typography from '@mui/material/Typography';
-import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import {  Routes, Route, Link, HashRouter} from "react-router-dom";
 import ErrorPage from './errorPage';
 import NewData from "./new";
 import {GetData} from "./getCurrentData";
@@ -12,27 +12,27 @@ function App() {
 
   return (
     
-    <BrowserRouter>
+    <HashRouter>
       <div className='header'>
         <Typography  variant="h1" gutterBottom style={{'fontFamily': 'Noto Serif  TC', 'fontWeight': '900', 'color':'red'}}>土壤濕度監測</Typography>
         <ButtonGroup variant="outlined" color="error">
-          <Button ><Link to="/soil-moisture" style={{'fontFamily': 'Noto Sans  TC', 'fontWeight': '400', 'color':'red'}}>Home Page</Link></Button>
-          <Button><Link to="/soil-moisture/Chart" style={{'fontFamily': 'Noto Sans  TC', 'fontWeight': '400', 'color':'red'}}> Chart</Link></Button>
-          <Button><Link to="/soil-moisture/History" style={{'fontFamily': 'Noto Sans  TC', 'fontWeight': '400', 'color':'red'}}> History</Link></Button>
+          <Button ><Link to="/" style={{'fontFamily': 'Noto Sans  TC', 'fontWeight': '400', 'color':'red'}}>Home Page</Link></Button>
+          <Button><Link to="/Chart" style={{'fontFamily': 'Noto Sans  TC', 'fontWeight': '400', 'color':'red'}}> Chart</Link></Button>
+          <Button><Link to="/History" style={{'fontFamily': 'Noto Sans  TC', 'fontWeight': '400', 'color':'red'}}> History</Link></Button>
         </ButtonGroup>
       </div> 
-      <Routes>
-        <Route path="/soil-moisture" element={<GetData/>} />
-        <Route path="/soil-moisture/Chart" element={<GetChart/>} />
-        <Route path="/soil-moisture/History" element={<GetAllData/>} />
-        <Route path="/soil-moisture/*" element={<ErrorPage/>} />
+      <Routes >
+        <Route path="/" element={<GetData/>} />
+        <Route path="/Chart" element={<GetChart/>} />
+        <Route path="/History" element={<GetAllData/>} />
+        <Route path="/*" element={<ErrorPage/>} />
       </Routes>
       <div className='footer'>
         <a href="https://github.com/LeeCH-OeO/soil-moisture" target="_blank" rel="noreferrer" >
           <Typography variant='subtitle2' style={{'fontFamily': 'Noto Serif  TC', 'fontWeight': '700', 'color':'red'}}>©LeeCH-OeO</Typography> 
         </a>
       </div>
-    </BrowserRouter>
+    </HashRouter>
     
   );
 }
