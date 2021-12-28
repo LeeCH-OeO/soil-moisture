b9a5b08bab3210368d38475d47eb91f9a22f7ec2
LeeCH-OeO <fairprice81000@gmail.com>
2021-12-28 23:18:48 +0800

    update chart

--- src/getChart.js
+++ src/getChart.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getDatabase, ref, onValue} from "firebase/database";
 import myFirebase from "./firebase";
-import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer} from 'recharts';
 
 const database = getDatabase(myFirebase)
 function GetChart(){
@@ -24,14 +24,16 @@ function GetChart(){
         
         <div>
             <div className="chart">
-                
-                    <AreaChart width={1800} height={700} data={sensorData}>
+                <ResponsiveContainer width="100%" height={450}>
+                    <AreaChart data={sensorData}>
                     <Area type="monotone" dataKey="humidity" stroke="#000000"  activeDot={{ r: 3 }}/>
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey="time"/>
                     <YAxis dataKey="humidity"/>
                     <Tooltip />
-                    </AreaChart>             
+                    </AreaChart>
+                </ResponsiveContainer>
+             
             </div>            
         </div>
     )
