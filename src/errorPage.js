1bea001683f25db80d833e1ac3c86b3ac1cbb81c
LeeCH-OeO <fairprice81000@gmail.com>
2022-01-02 02:18:14 +0800

    update image style

--- src/errorPage.js
+++ src/errorPage.js
@@ -3,7 +3,7 @@ export default  function ErrorPage(){
     return(
         <div className="error">
             <h2 style={{'fontFamily': 'Noto Sans  TC', 'fontWeight': '900'}}>Error! Page not found!</h2>
-            <img src="https://walfiegif.files.wordpress.com/2021/04/out-transparent-5.gif?w=600" ></img>
+            <img src="https://walfiegif.files.wordpress.com/2021/04/out-transparent-5.gif?w=600" style={{'max-width': '100%'}}></img>
         </div>
     )
 }
